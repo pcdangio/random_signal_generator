ebf8deb4d9b259f3910e31e775db02dec5fe1acd
Paul D'Angio <pcdangio@gmail.com>
2019-10-05 19:58:58 -0400

    Fixed bug in random generator initialization

--- src/impl.cpp
+++ src/impl.cpp
@@ -5,7 +5,7 @@ using namespace pcd;
 random_signal_generator::impl::impl()
 {
     // Set up random distributions and generators.
-    random_signal_generator::impl::random_generator = std::mt19937(random_device);
+    random_signal_generator::impl::random_generator = std::mt19937(random_signal_generator::impl::random_device());
     random_signal_generator::impl::distribution_zero_one = std::uniform_real_distribution<double>(0.0, 1.0);
     random_signal_generator::impl::distribution_pm_one = std::uniform_real_distribution<double>(-1.0, 1.0);
 }
