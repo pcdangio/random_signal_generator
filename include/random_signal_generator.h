93a664c4c6a30f2c5a7f78832b14cfe6760a5ba6
Paul D'Angio <pcdangio@gmail.com>
2019-10-07 22:12:20 -0400

    updated documentation and doxyfile

--- include/random_signal_generator.h
+++ include/random_signal_generator.h
@@ -18,16 +18,23 @@ public:
     // PARAMETERS
 
     /// \brief The number of points to create for the signal.
+    /// \details This specifies the length of the generated signal as a number of points.
     uint32_t n_points;
     /// \brief The desired mean of the signal.
+    /// \details This specifies the average amplitude of the generated signal.
     double desired_mean;
     /// \brief The desired standard deviation of the signal.
+    /// \details This specifies the standard deviation of the generated signal's amplitude from desired_mean.
     double desired_standard_deviation;
-    /// \brief The number of basis vectors used to create the signal.
+    /// \brief (OPTIONAL) The number of basis vectors used to create the signal. DEFAULT = 50
+    /// \details This controls the number of random sine waves that are superimposed to create the random signal.
+    /// More bases increase the complexity of the signal but takes longer to compute.
     uint8_t n_bases;
-    /// \brief The maximum frequency of the signal's content.
+    /// \brief (OPTIONAL) The maximum frequency of the signal's content, in Hz. DEFAULT = 10.0
+    /// \details This controls the maximum number of peaks/valleys in the signal over the specified number of points.
     double max_frequency;
-    /// \brief The amount of noise to superimpose on the signal, in percentage of the standard deviation.
+    /// \brief (OPTIONAL) The amount of noise to superimpose on the signal, in percentage of the desired standard deviation. DEFAULT = 0.0
+    /// \details This enables Gaussian (white) noise to be added to the random signal.
     double noise_percentage;
 
 
